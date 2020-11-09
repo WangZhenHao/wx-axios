eadc5ea0b55351cbe05e46ebebc3959c6fb95374
udream-WangZhenHao <wangzh@udream.cn>
2020-11-09 13:57:04 +0800

    fix: 案例修改引入路径

--- example/pages/index/index.js
+++ example/pages/index/index.js
@@ -1,7 +1,7 @@
 //index.js
 //获取应用实例
 const app = getApp()
-const axios = require('../../utils/dist/wx-axois.js')
+const axios = require('../../dist/wx-axois')
 console.log(axios.cancelToken.source())
 Page({
   onLoad: function () {
